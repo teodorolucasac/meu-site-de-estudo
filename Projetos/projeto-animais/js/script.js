8f5be5e4f8fe79c1fa0a5417dd0430837e5d628f
Lucas Teodoro <72241578+teodorolucasac@users.noreply.github.com>
2022-07-31 23:10:15 -0300

    Att Animais e Add Slide

--- Projetos/projeto-animais/js/script.js
+++ Projetos/projeto-animais/js/script.js
@@ -5,10 +5,11 @@ import initTabNav from './modules/tab-nav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
-import initMenuMobile from './modules/menu-monbile.js'
+import initMenuMobile from './modules/menu-mobile.js'
 import initFuncionamento from './modules/funcionamento.js'
 import initFetchAnimais from './modules/fetch-animais.js'
 import initFetchBitcoin from './modules/fetch-bitcoin.js'
+import SlideNav from "./modules/slide.js";
 
 
 initScrollSuave();
@@ -22,3 +23,8 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
